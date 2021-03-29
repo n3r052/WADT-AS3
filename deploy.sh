0cf4b4780d70d367f94772e0cc049d3b918ef65d
Haziq <19FTT157@student.pb.edu.bn>
2021-03-29 11:22:05 +0800

    edited deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:n3r052/19FTT1157.git master:gh-pages
+git push -f git@github.com:n3r052/WADT-AS3.git master:gh-pages
 cd -
